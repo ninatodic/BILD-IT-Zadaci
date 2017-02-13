167da8a934ee8862aba91c777f4753d7cc463841
ninatodic <man.who.wants.to.fly@gmail.com>
2017-02-13 09:19:00 +0100

    Ispravka prvog i drugog zadatka

--- src/zadaci_11_02_2017/Zadatak1SmallestFactors.java
+++ src/zadaci_11_02_2017/Zadatak1SmallestFactors.java
@@ -1,33 +1,48 @@
 package zadaci_11_02_2017;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Zadatak1SmallestFactors {
 
 	public static void main(String[] args) {
 		// create a scanner
-		Scanner input = new Scanner (System.in);
-		// prompt the user for the integer
-		System.out.println("Enter some integer: ");
-		int userInput = input.nextInt();
-		
-		// declare the arrayList and initialize it with invoking smallestFactors method
+		Scanner input = new Scanner(System.in);
+
+		int userInput = 0;
+		boolean ex = true;
+		// repeat prompt to the user for the integer until he enters it
+		do {
+			try {
+
+				System.out.println("Enter some integer: ");
+				userInput = input.nextInt();
+				ex = false;
+			} catch (InputMismatchException e) {
+				System.out.println("Invalid input, enter integer");
+				input.nextLine();
+			}
+		} while (ex);
+
+		// declare the arrayList and initialize it with invoking smallestFactors
+		// method
 		ArrayList<Integer> smallestFactors = smallestFactors(userInput);
-		
-		// print the array list that holds smallest factors 
+
+		// print the array list that holds smallest factors
 		for (int i = 0; i < smallestFactors.size(); i++) {
 			System.out.print(smallestFactors.get(i) + " ");
 		}
 
 		input.close();
 	}
-	
-	// Method that returns an array list that holds smallest factors of the integer
+
+	// Method that returns an array list that holds smallest factors of the
+	// integer
 	public static ArrayList<Integer> smallestFactors(int number) {
 		ArrayList<Integer> smallestFactors = new ArrayList<>();
-		int i = 2; 
-		while (number / i !=0) {
+		int i = 2;
+		while (number / i != 0) {
 			if (number % i == 0) {
 				number /= i;
 				smallestFactors.add(i);
--- src/zadaci_11_02_2017/Zadatak2ISBNumber.java
+++ src/zadaci_11_02_2017/Zadatak2ISBNumber.java
@@ -1,48 +1,61 @@
 package zadaci_11_02_2017;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Zadatak2ISBNumber {
 
 	public static void main(String[] args) {
 		// create a scanner
-		Scanner input = new Scanner (System.in);
+		Scanner input = new Scanner(System.in);
 		// prompt the user for the first 9 digits of ISBN-10 number
-		System.out.println("Enter first 9 digits of ISBN-10 number: ");
-		String isbn = input.next();
-		
+		String isbn = "";
+		boolean ex = true;
+		do {
+			try {
+
+				System.out.println("Enter first 9 digits of ISBN-10 number: ");
+				isbn = input.next();
+				int isbnInt = Integer.parseInt(isbn);
+				ex = false;
+			} catch (NumberFormatException e) {
+				System.out.println("Invalid input, try again");
+				input.nextLine();
+			}
+		} while (ex);
+
 		// invoke the method that prints whole ISBN-10 number
 		printISBN(isbn);
-		
-		//close scanner
+
+		// close scanner
 		input.close();
-		
+
 	}
-	
+
 	// Method that prints whole ISBN-10 number
-	public static void printISBN (String isbn){
+	public static void printISBN(String isbn) {
 		// array that holds first 9 numbers as digits
 		int[] digitsISBN = new int[9];
-		
+
 		// get nine digits from user input
-		for (int i = 0; i<9; i++){
+		for (int i = 0; i < 9; i++) {
 			int digit = Character.getNumericValue(isbn.charAt(i));
 			digitsISBN[i] = digit;
 		}
-		
-		// get the result from the formula for calculating 10th digit for the number
+
+		// get the result from the formula for calculating 10th digit for the
+		// number
 		int result = 0;
 		for (int i = 0; i < digitsISBN.length; i++) {
-			result += digitsISBN[i] * (i+1);
+			result += digitsISBN[i] * (i + 1);
 		}
-		
-		int checkSum = result%11;
+
+		int checkSum = result % 11;
 
 		// print one of two possible solutions
-		if (checkSum == 10){
+		if (checkSum == 10) {
 			System.out.println("The ISBN number is " + isbn + "X");
-		}
-		else{
+		} else {
 			System.out.println("The ISBN number is " + isbn + checkSum);
 		}
 	}
