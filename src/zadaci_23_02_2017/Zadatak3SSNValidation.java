5412e8d81a04f4054614e10354f22fc5b98348bb
ninatodic <man.who.wants.to.fly@gmail.com>
2017-02-26 12:02:15 +0100

    ispravljen 3 zadatak

--- src/zadaci_23_02_2017/Zadatak3SSNValidation.java
+++ src/zadaci_23_02_2017/Zadatak3SSNValidation.java
@@ -7,25 +7,28 @@ public class Zadatak3SSNValidation {
 	public static void main(String[] args) {
 		// create scanner and prompt the user for input
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.println("Enter Social Security Number in format DDD-DD-DDDD");
-		
+
 		String userInput = input.nextLine();
-		
+
 		// prints weather the input is valid ssn
-		if(isValidSSN(userInput)){
-			System.out.println("The number you entered is valid SSN");
-		}
-		else{
+
+		while (!isValidSSN(userInput)) {
 			System.out.println("The number you entered is not valid SSN");
+			System.out.println("Enter Social Security Number in format DDD-DD-DDDD");
+			userInput = input.nextLine();
+			if (isValidSSN(userInput)) {
+				System.out.println("The number you entered is valid SSN");
+			}
 		}
-		
+
 		input.close();
 	}
 
-	// method that returns true if format of input matches to format of ssn 
+	// method that returns true if format of input matches to format of ssn
 	public static boolean isValidSSN(String userInput) {
-		if(userInput.matches("\\d{3}-\\d{2}-\\d{4}")){
+		if (userInput.matches("\\d{3}-\\d{2}-\\d{4}")) {
 			return true;
 		}
 		return false;
