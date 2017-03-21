1d02e5ee51f4ce2820fcfb8304f1719fa10f01a2
ninatodic <man.who.wants.to.fly@gmail.com>
2017-03-21 17:39:35 +0100

    dodat exception

--- src/zadaci_18_03_2017/Zadatak5LargestNumberInArray.java
+++ src/zadaci_18_03_2017/Zadatak5LargestNumberInArray.java
@@ -1,5 +1,6 @@
 package zadaci_18_03_2017;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Zadatak5LargestNumberInArray {
@@ -9,17 +10,26 @@ public class Zadatak5LargestNumberInArray {
 		Scanner input = new Scanner(System.in);
 
 		// Prompt the user to enter a list of eight integers
-		System.out.print("Enter eight integers: ");
 		int[] list = new int[8]; // Create array of eight integers
-		for (int i = 0; i < list.length; i++) {
-			list[i] = input.nextInt();
-		}
-
+		boolean ex = true;
+		do {
+			try {
+				System.out.print("Enter eight integers: ");
+				for (int i = 0; i < list.length; i++) {
+					list[i] = input.nextInt();
+				}
+				ex = false;
+			} catch (InputMismatchException e) {
+				System.out.println("Invalid input, try again");
+				input.nextLine();
+			}
+		} while (ex);
 		// Display the largest element
 		System.out.println("The largest element is " + max(list));
+		input.close();
 	}
 
-	/** Method finds the largest number in an array */ 
+	/** Method finds the largest number in an array */
 	public static int max(int[] list) {
 		int max = list[list.length - 1]; // Largest number
 		int index = list.length - 1; // List index
@@ -30,10 +40,9 @@ public class Zadatak5LargestNumberInArray {
 	private static int max(int[] list, int index, int max) {
 		if (index < 0) // Base case
 			return max;
-		else if (list[index] > max) { 
+		else if (list[index] > max) {
 			return max(list, index - 1, list[index]); // Recursive call
-		}
-		else
+		} else
 			return max(list, index - 1, max); // Recursive call
 	}
 
